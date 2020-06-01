09f45553428c085526db5416b3539bf7283e6c68
SimoneGiacomini <simogiaco77@gmail.com>
2020-06-01 14:39:52 +0200

    prova

--- Carta_Alta/src/carta/Main.java
+++ Carta_Alta/src/carta/Main.java
@@ -9,7 +9,7 @@ public class Main {
 		Mazzo mazzoIta= new Mazzo(TipoMazzo.TEDESCO) ;
 		//Character[] semi= new Character[]{'\u2660',	'\u2663',	'\u2665',	'\u2666'};
 		while (true) {
-		InputDati.isInvioPremuto(mazzoIta.estrai().toString());
+		InputDati.isInvioPremuto(mazzoIta.estrai());
 		}
 	}
 	
